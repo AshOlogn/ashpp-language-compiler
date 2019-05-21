44c21fc4f46a62d1d62a0a798964841b6d7a1478
Ashwin Devaraj <ashwin.dev.pro@gmail.com>
2019-05-21 18:48:12 -0500

    print 3-address code generated

--- main.ml
+++ main.ml
@@ -5,6 +5,8 @@ open Err
 open Checker
 open Symtable
 open Opt_const_fold
+open Three_address
+open Gen_three_address
 
 (* copied some boilerplate from Real World OCaml textbook for file IO *)
 (* http://dev.realworldocaml.org/parsing-with-ocamllex-and-menhir.html#defining-a-lexer *)
@@ -15,11 +17,13 @@ let parse_with_error lexbuf =
   let (stat, _) = check_stat ((Parser.main Lexer.read lexbuf), symtable_init) in
   let (stat', _) = (const_fold_stat (stat, symtable_init)) in
   let stat'' = prune_empty stat' in
-  (Printf.printf "%s\n" (show_stat stat'')) with
+  let (three_ins, _, _) = gen_three_address_stat stat'' 0 0 in
+  (Printf.printf "%s\n" (show_stat stat''));
+  (Printf.printf "%s" (show_three_instructions three_ins)) with
   | LexerError msg 
   | TypeError msg 
   | ScopeError msg 
-  | ReturnError msg -> Printf.printf "%s" msg 
+  | ReturnError msg -> Printf.printf "%s" msg
 
 let () = 
   
